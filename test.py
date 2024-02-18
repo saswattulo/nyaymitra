f9083c5960286818ef8d7aed5f2f98486ad720bd
Saswat Tulo <106906965+saswattulo@users.noreply.github.com>
2024-02-18 22:45:28 +0530

    Update test.py
    Added main function

--- test.py
+++ test.py
@@ -20,7 +20,8 @@ os.environ['GRADIENT_WORKSPACE_ID'] = "4de36c1f-5ee6-41da-8f95-9d2fb1ded33a_work
 
 st.set_page_config(page_title = "NyayMitra", page_icon="⚖︎")
 st.header('Chat with your legal companion NyayMitra')
-
+        
+@st.cache_resource
 def create_datastax_connection():
 
     cloud_config = {
@@ -52,81 +53,85 @@ def read_pdf_from_directory(directory):
                 pdf_texts.append(text)
     return pdf_texts
 
-# Define the local directory containing PDF files
+def main():
 
-pdf_directory = os.path.dirname(os.path.abspath(__file__))
+    # Define the local directory containing PDF files
+    pdf_directory = os.path.dirname(os.path.abspath(__file__))
 
-st.subheader('Processing PDF Files from Local Directory')
-if st.button('Start'):
-    with st.spinner('Starting bot...'):
-        pdf_texts = read_pdf_from_directory(pdf_directory)
+    st.subheader('Processing PDF Files from Local Directory')
+    if st.button('Start'):
+        with st.spinner('Starting bot...'):
+            pdf_texts = read_pdf_from_directory(pdf_directory)
 
-        if pdf_texts:
-            st.success(f"Processed {len(pdf_texts)} PDF file(s) from the directory.")
-            if "conversation" not in st.session_state:
-                st.session_state.conversation = None
+            if pdf_texts:
+                st.success(f"Processed {len(pdf_texts)} PDF file(s) from the directory.")
+                if "conversation" not in st.session_state:
+                    st.session_state.conversation = None
 
-            if "activate_chat" not in st.session_state:
-                st.session_state.activate_chat = False
+                if "activate_chat" not in st.session_state:
+                    st.session_state.activate_chat = False
 
-            if "messages" not in st.session_state:
-                st.session_state.messages = []
+                if "messages" not in st.session_state:
+                    st.session_state.messages = []
 
-            for message in st.session_state.messages:
-                with st.chat_message(message["role"], avatar = message['avatar']):
-                    st.markdown(message["content"])
+                for message in st.session_state.messages:
+                    with st.chat_message(message["role"], avatar = message['avatar']):
+                        st.markdown(message["content"])
 
-            session=create_datastax_connection()
+                session=create_datastax_connection()
 
-            llm = GradientBaseModelLLM(base_model_slug="llama2-7b-chat", max_tokens=400)
+                llm = GradientBaseModelLLM(base_model_slug="llama2-7b-chat", max_tokens=400)
 
-            embed_model = GradientEmbedding(
-            gradient_access_token = os.environ["GRADIENT_ACCESS_TOKEN"],
-            gradient_workspace_id = os.environ["GRADIENT_WORKSPACE_ID"],
-            gradient_model_slug="bge-large")
+                embed_model = GradientEmbedding(
+                gradient_access_token = os.environ["GRADIENT_ACCESS_TOKEN"],
+                gradient_workspace_id = os.environ["GRADIENT_WORKSPACE_ID"],
+                gradient_model_slug="bge-large")
 
-            service_context = ServiceContext.from_defaults(
-            llm = llm,
-            embed_model = embed_model,
-            chunk_size=256)
+                service_context = ServiceContext.from_defaults(
+                llm = llm,
+                embed_model = embed_model,
+                chunk_size=256)
 
-            set_global_service_context(service_context)
+                set_global_service_context(service_context)
 
-            #reader = SimpleDirectoryReader("Z:\exp\env_chat_bot")
-            # Get the current directory
-            reader = os.path.dirname(os.path.abspath(__file__))
-            
+                #reader = SimpleDirectoryReader("Z:\exp\env_chat_bot")
+                # Get the current directory
+                reader = os.path.dirname(os.path.abspath(__file__))
+                
 
-            # Load data from the directory
-            documents = reader.load_data()
-            index = VectorStoreIndex.from_documents(documents,
-                                                            service_context=service_context) #This line creates an index from the loaded documents.
-            query_engine = index.as_query_engine() #This line converts the index to a query engine.
-            if "query_engine" not in st.session_state:
-                st.session_state.query_engine = query_engine
-            st.session_state.activate_chat = True
+                # Load data from the directory
+                documents = reader.load_data()
+                index = VectorStoreIndex.from_documents(documents,
+                                                                service_context=service_context) #This line creates an index from the loaded documents.
+                query_engine = index.as_query_engine() #This line converts the index to a query engine.
+                if "query_engine" not in st.session_state:
+                    st.session_state.query_engine = query_engine
+                st.session_state.activate_chat = True
 
-            if st.session_state.activate_chat == True:
-                if prompt := st.chat_input("Ask your query"):
-                    if prompt := st.chat_input("Ask your question from the PDF?"):
-                        with st.chat_message("user", avatar = '👨🏻'):
-                            st.markdown(prompt)
-                        st.session_state.messages.append({"role": "user", 
-                                              "avatar" :'👨🏻',
-                                              "content": prompt})
-                        
-                        query_index_placeholder = st.session_state.query_engine
-                        pdf_response = query_index_placeholder.query(prompt)
-                        cleaned_response = pdf_response.response
-                        with st.chat_message("assistant", avatar='🤖'):
-                            st.markdown(cleaned_response)
-                        st.session_state.messages.append({"role": "assistant", 
-                                              "avatar" :'🤖',
-                                              "content": cleaned_response})
-                        
+                if st.session_state.activate_chat == True:
+                    if prompt := st.chat_input("Ask your query"):
+                        if prompt := st.chat_input("Ask your question from the PDF?"):
+                            with st.chat_message("user", avatar = '👨🏻'):
+                                st.markdown(prompt)
+                            st.session_state.messages.append({"role": "user", 
+                                                "avatar" :'👨🏻',
+                                                "content": prompt})
+                            
+                            query_index_placeholder = st.session_state.query_engine
+                            pdf_response = query_index_placeholder.query(prompt)
+                            cleaned_response = pdf_response.response
+                            with st.chat_message("assistant", avatar='🤖'):
+                                st.markdown(cleaned_response)
+                            st.session_state.messages.append({"role": "assistant", 
+                                                "avatar" :'🤖',
+                                                "content": cleaned_response})
+                            
 
-            
-                   
-            # Now you can use pdf_texts for further processing or display
-        else:
-            st.warning("No PDF files found in the directory.")
+                
+                    
+                
+            else:
+                st.warning("No PDF files found in the directory.")
+
+if __name__ == "__main__":
+    main()
